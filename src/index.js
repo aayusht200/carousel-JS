d36d0fba14d5a3110b12d346c3f283e690c2a3ce
aayusht200 <aayush.t200@gmail.com>
2025-11-17 15:06:55 +0530

    minimal fixes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import "./style.css"
-const buttons = document.querySelectorAll(".carousel-button")
+import './style.css';
+const buttons = document.querySelectorAll('.carousel-button');
 
 buttons.forEach((button) => {
-	button.addEventListener("click", () => {
-		const offset = button.dataset.carouselButton === "next" ? 1 : -1
-		const slide = button
-			.closest("[data-carousel]")
-			.querySelector("[data-slide]")
-		const current = slide.querySelector("[data-active]")
-		let newIndex = [...slide.children].indexOf(current) + offset
-		if (newIndex < 0) newIndex = slide.children.length - 1
-		if (newIndex >= slide.children.length) newIndex = 0
-		slide.children[newIndex].dataset.active = true
-		delete current.dataset.active
-	})
-})
+    button.addEventListener('click', () => {
+        const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+        const slide = button.closest('[data-carousel]').querySelector('[data-slides]');
+        const current = slide.querySelector('[data-active]');
+        let newIndex = [...slide.children].indexOf(current) + offset;
+        if (newIndex < 0) newIndex = slide.children.length - 1;
+        if (newIndex >= slide.children.length) newIndex = 0;
+        slide.children[newIndex].dataset.active = true;
+        delete current.dataset.active;
+    });
+});
